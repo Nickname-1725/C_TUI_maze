e0010f37c91711cf4c6d3a1cfe78a579e7603127
Nickname-1725 <1195019369@qq.com>
2024-03-23 22:42:44 +0800

    增加了创建窗口并打印迷宫的代码

--- rendering.c
+++ rendering.c
@@ -13,8 +13,9 @@ char* cross_list[] = {"  ", "╙ ", "═ ", "╝ ",
                       "╔═", "╠═", "╦═", "╬═"};
 char* maze_string (Table* table) {
   Coordinate coordinate = {0,0};
-  char* str = malloc(sizeof (char) * (table->h_maze * ( 1 + table->w_maze * 2 * 4) + 1));
-  strcpy (str, "");
+  // 行数 x (行首空格x1 + 行末换行x1 + 列数 x 每格字符x2 x Unicode占字节x4) + 休止符x1
+  char* str = malloc(sizeof (char) * (table->h_maze * ( 1 + 1 + table->w_maze * 2 * 4) + 1));
+  strcpy (str, " "); // 行首空格
   for (coordinate.i = 0; coordinate.i < table->h_maze; coordinate.i++) {
     for (coordinate.j = 0; coordinate.j < table->w_maze; coordinate.j++) {
       int cross_index = 0;
@@ -28,7 +29,7 @@ char* maze_string (Table* table) {
       }
       strcat (str, cross_list[cross_index]);
     }
-    strcat (str, "\n");
+    strcat (str, "\n "); // 换行 + 空格
   }
   return str;
 }
@@ -51,8 +52,14 @@ int main () {
   Coordinate kernel = {0,1};
   maze_realize (table, &kernel);
 
+  WINDOW* win = newwin(9, 20, 1, 1); // w_maze * 2 + 2 (行首空格, 换行符)
+  refresh(); // 必须refresh(), 否则无法显示非并标准窗口
+  box(win, 0, 0);
+
   char* str = maze_string (table);
-  printw ("%s", str);
+  wprintw (win, "%s", str);
+  //printw ("%s", str);
+  wrefresh(win);
 
   getch();
 
